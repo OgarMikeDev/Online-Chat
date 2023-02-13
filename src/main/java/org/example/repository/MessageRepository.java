d833c33df4b1403ddb49db0b79474f800e253f1c
VitSens <55030208+VitSens@users.noreply.github.com>
2023-02-13 10:56:48 +0300

    make repository for models

--- src/main/java/org/example/repository/MessageRepository.java
+++ src/main/java/org/example/repository/MessageRepository.java
@@ -1,8 +1,10 @@
-package main.model;
+package org.example.repository;
 
+import org.example.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Integer> {
+
 }
